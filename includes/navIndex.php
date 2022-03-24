1372a309cac060ca92effd8436cfedeb9d7c75bc
Martin VF <mvilasanchezf@alumnos.nebrija.es>
2022-03-24 19:11:07 +0100

    Somos Responsive

--- includes/navIndex.php
+++ includes/navIndex.php
@@ -6,8 +6,8 @@
         <div class="news"><a href="news.php">Novedades</a></div>
         <div class="buy"><a href="buy.php">Comprar</a></div>
         <div class="sell"><a href="sell.php">Vender</a></div>
-        <div class="lastdays"><a href="lastdays.php">Últimos Días</a></div>
-        <div class="maintenance"><a href="mainteinance.php">Mantenimiento de Vehículo</a></div>
+        <div class="lastdays"><a href="lastdays.php">LastDays</a></div>
+        <div class="maintenance"><a href="mainteinance.php">Mantenimiento</a></div>
         <div class="contact"><a href="contact.php">Contacto</a></div>
     </div>
 </nav>
