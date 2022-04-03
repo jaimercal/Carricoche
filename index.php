9c03d979d398f9f232f5b1dafb53127cb2523f2f
Martin VF <mvilasanchezf@alumnos.nebrija.es>
2022-04-03 23:56:57 +0200

    Intento de slider

--- index.php
+++ index.php
@@ -15,7 +15,39 @@
 include "includes/header.php";
 include "includes/navIndex.php";
 ?>
-<section></section>
+<section>
+    <h2>BIENVENIDO</h2>
+    <p>Compra venta de vehiculos de segunda mano</p>
+
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="img/imgSlider/lateral.jpg" style="width:100%">
+            <div class="text">Caption Text</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="img/imgSlider/frontal.jpg" style="width:100%">
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="img/imgSlider/libre.jpg" style="width:100%">
+            <div class="text">Caption Three</div>
+        </div>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+</section>
 <?php
 include "includes/footer.php";
 ?>
